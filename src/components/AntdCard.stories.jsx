b27521d66b26d85afd37a58a2279635eb8e3714c
Giorbis Miguel <giorbismiguel@gmail.com>
2021-11-04 23:06:56 -0400

    Add Ant Design

---
+++ src/components/AntdCard.stories.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import { AntdCard } from './AntdCard';
+
+export default {
+  title: "AntD Card",
+  component: AntdCard,
+};
+
+export const DefaultState = () => <AntdCard />;
+
